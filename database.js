70196e8719d8db5eed2760e75236663c116d3b68
R4Qabdi <jadepitkapswen@gmail.com>
2026-05-23 11:37:25 +0800

    try again

--- database.js
+++ database.js
@@ -1,13 +1,23 @@
-import 'dotenv/config'
+// import 'dotenv/config'
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import pg from 'pg' // You might need to install this: npm install pg
+// import { PrismaClient } from '@prisma/client'
+
+// const connectionString = process.env.DATABASE_URL
+// const pool = new pg.Pool({ connectionString })
+// const adapter = new PrismaPg(pool)
+
+// // Pass the adapter here instead of datasourceUrl
+// const prisma = new PrismaClient({ adapter })
+
+// export default prisma
+
 import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg' // You might need to install this: npm install pg
+import pg from 'pg'
 import { PrismaClient } from '@prisma/client'
 
-const connectionString = process.env.DATABASE_URL
-const pool = new pg.Pool({ connectionString })
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
-
-// Pass the adapter here instead of datasourceUrl
 const prisma = new PrismaClient({ adapter })
 
 export default prisma
--- database/config.database.js
+++ database/config.database.js
@@ -1,16 +1,26 @@
-import 'dotenv/config'
+// import 'dotenv/config'
+// import { PrismaPg } from '@prisma/adapter-pg'
+// import pg from 'pg' // You might need to install this: npm install pg
+// // import { PrismaClient } from '../generated/prisma/client.js
+
+// import prismaClientPkg from '@prisma/client'
+// const { PrismaClient } = prismaClientPkg
+
+// const connectionString = process.env.DATABASE_URL
+// const pool = new pg.Pool({ connectionString })
+// const adapter = new PrismaPg(pool)
+
+// // Pass the adapter here instead of datasourceUrl
+// const prisma = new PrismaClient({ adapter })
+
+// export default prisma 
+
 import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg' // You might need to install this: npm install pg
-// import { PrismaClient } from '../generated/prisma/client.js
+import pg from 'pg'
+import { PrismaClient } from '@prisma/client'
 
-import prismaClientPkg from '@prisma/client'
-const { PrismaClient } = prismaClientPkg
-
-const connectionString = process.env.DATABASE_URL
-const pool = new pg.Pool({ connectionString })
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
 const adapter = new PrismaPg(pool)
-
-// Pass the adapter here instead of datasourceUrl
 const prisma = new PrismaClient({ adapter })
 
-export default prisma 
+export default prisma
