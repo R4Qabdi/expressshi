442abfcbcfd7ef3f2290a27bf156020023ce0996
R4Qabdi <jadepitkapswen@gmail.com>
2026-05-23 10:25:20 +0800

    meet 7 auth

--- routes/index.route.js
+++ routes/index.route.js
@@ -4,6 +4,8 @@ import booksRoute from './books.route.js'
 import usersRoute from './users.route.js'
 import profileRoute from './profiles.route.js'
 import borrowingRoute from './borrowings.route.js'
+import authRoute from './auth.route.js'
+import { authenticateToken } from '../middlewares/auth.middleware.js'
 
 const router = express.Router()
 
@@ -11,9 +13,11 @@ router.get('/', (req, res) => {
   res.send('Welcome to the API Library')
 })
 
-router.use('/books', booksRoute)
-router.use('/users', usersRoute)
-router.use('/profiles', profileRoute)
-// router.use('/borrowings', borrowingsRoute)
+router.use('/auth', authRoute)
+router.use('/books', authenticateToken, booksRoute)
+router.use('/users', authenticateToken, usersRoute)
+router.use('/profiles', authenticateToken, profileRoute)
+router.use('/auth', authRoute)
+router.use('/borrowings', authenticateToken, borrowingRoute)
 
 export default router
