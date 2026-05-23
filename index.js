cd6b9fa95cef193b8e58911c34d275dc6571044a
R4Qabdi <jadepitkapswen@gmail.com>
2026-05-23 11:00:13 +0800

    fix crashes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 // import prisma from './configs/database.js' dihapus karena sudah diimport di masing-masing route
 import router from './routes/index.route.js'
 
+
 const app = express()
 const port = 3000
 
@@ -13,8 +14,8 @@ if (process.env.ENV !== 'production') {
   const port = process.env.PORT || 3000
 
   app.listen(port, () => {
-    logger.info(`Library API is running at http://localhost:${port}`)
-    logger.info('Application started successfully')
+    console.log(`Library API is running at http://localhost:${port}`)
+    console.log('Application started successfully')
   })
 }
 
